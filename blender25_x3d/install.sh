c2eebc414bee2c243dcc62b2e2ba78416b78f501
Michalis Kamburelis <michalis.kambi@gmail.com>
2011-05-15 17:43:42 +0000

    Update install.sh for Blender 2.57 path.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/blender@8908 afe8dcc5-9094-451b-8773-5f495042a737

--- blender25_x3d/install.sh
+++ blender25_x3d/install.sh
@@ -7,4 +7,4 @@ set -eu
 # (what is the generic dir name under $HOME, regardless of Blender version,
 # that works with Blender 2.5x?)
 
-cp -f export_x3d.py ~/installed/blender-2.56a-beta-linux-glibc27-i686/2.56/scripts/op/io_scene_x3d/export_x3d.py
+cp -f export_x3d.py ~/installed/blender/2.57/scripts/addons/io_scene_x3d/export_x3d.py
