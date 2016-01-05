2fa6eb3349eb6eed5f136369631b16910f33b75b
Michalis Kamburelis <michalis.kambi@gmail.com>
2016-01-05 00:00:53 +0000

    Update Blender X3D export script. Fix shadeless implementation.
    git-svn-id: https://svn.code.sf.net/p/castle-engine/code/trunk/blender@15036 afe8dcc5-9094-451b-8773-5f495042a737

--- blender25_x3d/export_x3d.py
+++ blender25_x3d/export_x3d.py
@@ -41,32 +41,6 @@ import mathutils
 
 from bpy_extras.io_utils import create_derived_objects, free_derived_objects
 
-x3d_names_reserved = {'Anchor', 'Appearance', 'Arc2D', 'ArcClose2D', 'AudioClip', 'Background', 'Billboard',
-                      'BooleanFilter', 'BooleanSequencer', 'BooleanToggle', 'BooleanTrigger', 'Box', 'Circle2D',
-                      'Collision', 'Color', 'ColorInterpolator', 'ColorRGBA', 'component', 'Cone', 'connect',
-                      'Contour2D', 'ContourPolyline2D', 'Coordinate', 'CoordinateDouble', 'CoordinateInterpolator',
-                      'CoordinateInterpolator2D', 'Cylinder', 'CylinderSensor', 'DirectionalLight', 'Disk2D',
-                      'ElevationGrid', 'EspduTransform', 'EXPORT', 'ExternProtoDeclare', 'Extrusion', 'field',
-                      'fieldValue', 'FillProperties', 'Fog', 'FontStyle', 'GeoCoordinate', 'GeoElevationGrid',
-                      'GeoLocationLocation', 'GeoLOD', 'GeoMetadata', 'GeoOrigin', 'GeoPositionInterpolator',
-                      'GeoTouchSensor', 'GeoViewpoint', 'Group', 'HAnimDisplacer', 'HAnimHumanoid', 'HAnimJoint',
-                      'HAnimSegment', 'HAnimSite', 'head', 'ImageTexture', 'IMPORT', 'IndexedFaceSet',
-                      'IndexedLineSet', 'IndexedTriangleFanSet', 'IndexedTriangleSet', 'IndexedTriangleStripSet',
-                      'Inline', 'IntegerSequencer', 'IntegerTrigger', 'IS', 'KeySensor', 'LineProperties', 'LineSet',
-                      'LoadSensor', 'LOD', 'Material', 'meta', 'MetadataDouble', 'MetadataFloat', 'MetadataInteger',
-                      'MetadataSet', 'MetadataString', 'MovieTexture', 'MultiTexture', 'MultiTextureCoordinate',
-                      'MultiTextureTransform', 'NavigationInfo', 'Normal', 'NormalInterpolator', 'NurbsCurve',
-                      'NurbsCurve2D', 'NurbsOrientationInterpolator', 'NurbsPatchSurface',
-                      'NurbsPositionInterpolator', 'NurbsSet', 'NurbsSurfaceInterpolator', 'NurbsSweptSurface',
-                      'NurbsSwungSurface', 'NurbsTextureCoordinate', 'NurbsTrimmedSurface', 'OrientationInterpolator',
-                      'PixelTexture', 'PlaneSensor', 'PointLight', 'PointSet', 'Polyline2D', 'Polypoint2D',
-                      'PositionInterpolator', 'PositionInterpolator2D', 'ProtoBody', 'ProtoDeclare', 'ProtoInstance',
-                      'ProtoInterface', 'ProximitySensor', 'ReceiverPdu', 'Rectangle2D', 'ROUTE', 'ScalarInterpolator',
-                      'Scene', 'Script', 'Shape', 'SignalPdu', 'Sound', 'Sphere', 'SphereSensor', 'SpotLight', 'StaticGroup',
-                      'StringSensor', 'Switch', 'Text', 'TextureBackground', 'TextureCoordinate', 'TextureCoordinateGenerator',
-                      'TextureTransform', 'TimeSensor', 'TimeTrigger', 'TouchSensor', 'Transform', 'TransmitterPdu',
-                      'TriangleFanSet', 'TriangleSet', 'TriangleSet2D', 'TriangleStripSet', 'Viewpoint', 'VisibilitySensor',
-                      'WorldInfo', 'X3D', 'XvlShell', 'VertexShader', 'FragmentShader', 'MultiShaderAppearance', 'ShaderAppearance'}
 
 # h3d defines
 H3D_TOP_LEVEL = 'TOP_LEVEL_TI'
@@ -101,54 +75,55 @@ def clean_def(txt):
     # no digit start
     if txt[0] in "1234567890+-":
         txt = "_" + txt
-    return txt.translate({  # control characters 0x0-0x1f
-                            # 0x00: "_",
-                          0x01: "_",
-                          0x02: "_",
-                          0x03: "_",
-                          0x04: "_",
-                          0x05: "_",
-                          0x06: "_",
-                          0x07: "_",
-                          0x08: "_",
-                          0x09: "_",
-                          0x0a: "_",
-                          0x0b: "_",
-                          0x0c: "_",
-                          0x0d: "_",
-                          0x0e: "_",
-                          0x0f: "_",
-                          0x10: "_",
-                          0x11: "_",
-                          0x12: "_",
-                          0x13: "_",
-                          0x14: "_",
-                          0x15: "_",
-                          0x16: "_",
-                          0x17: "_",
-                          0x18: "_",
-                          0x19: "_",
-                          0x1a: "_",
-                          0x1b: "_",
-                          0x1c: "_",
-                          0x1d: "_",
-                          0x1e: "_",
-                          0x1f: "_",
+    return txt.translate({
+        # control characters 0x0-0x1f
+        # 0x00: "_",
+        0x01: "_",
+        0x02: "_",
+        0x03: "_",
+        0x04: "_",
+        0x05: "_",
+        0x06: "_",
+        0x07: "_",
+        0x08: "_",
+        0x09: "_",
+        0x0a: "_",
+        0x0b: "_",
+        0x0c: "_",
+        0x0d: "_",
+        0x0e: "_",
+        0x0f: "_",
+        0x10: "_",
+        0x11: "_",
+        0x12: "_",
+        0x13: "_",
+        0x14: "_",
+        0x15: "_",
+        0x16: "_",
+        0x17: "_",
+        0x18: "_",
+        0x19: "_",
+        0x1a: "_",
+        0x1b: "_",
+        0x1c: "_",
+        0x1d: "_",
+        0x1e: "_",
+        0x1f: "_",
 
-                          0x7f: "_",  # 127
+        0x7f: "_",  # 127
 
-                          0x20: "_",  # space
-                          0x22: "_",  # "
-                          0x27: "_",  # '
-                          0x23: "_",  # #
-                          0x2c: "_",  # ,
-                          0x2e: "_",  # .
-                          0x5b: "_",  # [
-                          0x5d: "_",  # ]
-                          0x5c: "_",  # \
-                          0x7b: "_",  # {
-                          0x7d: "_",  # }
-                          })
+        0x20: "_",  # space
+        0x22: "_",  # "
+        0x27: "_",  # '
+        0x23: "_",  # #
+        0x2c: "_",  # ,
+        0x2e: "_",  # .
+        0x5b: "_",  # [
+        0x5d: "_",  # ]
+        0x5c: "_",  # \
+        0x7b: "_",  # {
+        0x7d: "_",  # }
+        })
 
 
 def build_hierarchy(objects):
@@ -999,9 +974,13 @@ def export(file,
             transp = 1.0 - material.alpha
 
             if material.use_shadeless:
-                ambient = 1.0
+            	# Make the material purely emissive.
+                # This is optimized in Castle Game Engine and view3dscene.
+                ambient = 0.0
                 shininess = 0.0
-                specColor = emitColor = diffuseColor
+                emitColor = diffuseColor
+                diffuseColor = 0.0, 0.0, 0.0
+                specColor = 0.0, 0.0, 0.0
 
             ident_step = ident + (' ' * (-len(ident) + \
             fw('%s<Material ' % ident)))
@@ -1411,16 +1390,17 @@ def export(file,
     # Export Object Hierarchy (recursively called)
     # -------------------------------------------------------------------------
     def export_object(ident, obj_main_parent, obj_main, obj_children):
+        matrix_fallback = mathutils.Matrix()
         world = scene.world
         free, derived = create_derived_objects(scene, obj_main)
 
         if use_hierarchy:
             obj_main_matrix_world = obj_main.matrix_world
             if obj_main_parent:
-                obj_main_matrix = obj_main_parent.matrix_world.inverted() * obj_main_matrix_world
+                obj_main_matrix = obj_main_parent.matrix_world.inverted(matrix_fallback) * obj_main_matrix_world
             else:
                 obj_main_matrix = obj_main_matrix_world
-            obj_main_matrix_world_invert = obj_main_matrix_world.inverted()
+            obj_main_matrix_world_invert = obj_main_matrix_world.inverted(matrix_fallback)
 
             obj_main_id = quoteattr(unique_name(obj_main, obj_main.name, uuid_cache_object, clean_func=clean_def, sep="_"))
 
--- blender25_x3d/install.sh
+++ blender25_x3d/install.sh
@@ -6,7 +6,7 @@ set -eu
 # If you know how to make it generally usable, report
 # (what is the generic dir name under $HOME, regardless of Blender version?).
 
-BLENDER_VERSION='2.68'
+BLENDER_VERSION='2.76'
 
 cp -f export_x3d.py ~/installed/blender/"$BLENDER_VERSION"/scripts/addons/io_scene_x3d/export_x3d.py
 cp -f export_kanim.py ~/installed/blender/"$BLENDER_VERSION"/scripts/addons/export_kanim.py
