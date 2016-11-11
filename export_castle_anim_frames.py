3524b61367d4f014c2e1a31d4c4c1dbafb935058
Michalis Kamburelis <michalis.kambi@gmail.com>
2016-11-11 18:43:41 +0100

    Renamed kanim -> castle-anim-frames

--- export_castle_anim_frames.py
+++ export_castle_anim_frames.py
@@ -16,29 +16,23 @@
 #
 # ##### END GPL LICENSE BLOCK #####
 
-# This script exports to KAnim format,
-# which stands for Castle Game Engine's animations.
+# This script exports from Blender to castle-anim-frames format,
+# which stands for "Castle Game Engine's Animation Frames".
 # The format specification is on
-# http://castle-engine.sourceforge.net/kanim_format.php
-# Each still frame is exported to a separate X3D file.
+# http://castle-engine.sourceforge.net/castle_animation_frames.php
+# Each still frame is exported to a separate X3D graph.
 # We call actual Blender X3D exporter to do this.
 #
 # The latest version of this script can be found on
-# http://castle-engine.sourceforge.net/blender.php
-#
-# Note: Eventually, we would like to implement exporting animations
-# inside normal Blender X3D exporter. Our Castle Game Engine
-# can already perfectly read and animate normal X3D models. When this
-# happens, this script will stop being useful, as exporting whole animation
-# to a single X3D file will be much better than using KAnim.
+# http://castle-engine.sourceforge.net/creating_data_blender.php
 
 bl_info = {
-    "name": "Export KAnim",
-    "description": "Export animation to KAnim format (Castle Game Engine's animations).",
+    "name": "Export Castle Animation Frames",
+    "description": "Export animation to Castle Game Engine's Animation Frames format.",
     "author": "Michalis Kamburelis",
     "version": (1, 0),
     "blender": (2, 64, 0),
-    "location": "File > Export > Export KAnim (.kanim)",
+    "location": "File > Export > Castle Animation Frames (.castle-anim-frames)",
     "warning": "", # used for warning icon and text in addons panel
     # Note: this should only lead to official Blender wiki.
     # But since this script (probably) will not be official part of Blender,
@@ -52,19 +46,26 @@ import os
 from bpy_extras.io_utils import path_reference_mode
 from bpy.props import *
 
-class ExportKAnim(bpy.types.Operator):
-    """Export the animation to KAnim format (Castle Game Engine's animations)"""
-    bl_idname = "export.kanim"
-    bl_label = "Export KAnim (.kanim)"
+class ExportCastleAnimFrames(bpy.types.Operator):
+    """Export the animation to Castle Animation Frames (castle-anim-frames) format"""
+    bl_idname = "export.castle_anim_frames"
+    bl_label = "Castle Animation Frames (.castle-anim-frames)"
 
     # properties for interaction with fileselect_add
     filepath = StringProperty(subtype="FILE_PATH")
-    filter_glob = StringProperty(default="*.kanim", options={'HIDDEN'})
+    filter_glob = StringProperty(default="*.castle-anim-frames", options={'HIDDEN'})
 
-    # properties special for KAnim export
+    # properties special for castle-anim-frames export
     frame_skip = IntProperty(name="Frames to skip",
-        # As part of exporting to KAnim, we export each still frame to X3D. We iterate over all animation frames, from the start, exporting it and skipping this number of following frames. Smaller values mean less files (less disk usage, faster animation loading in game) but also worse quality (as KAnim loader in game only interpolates linearly between frames). Default is 4, which means every 5th frame is exported, which means 5 frames for each second (for default 25fps)
-        description="How many frames to skip between exported frames. The game using KAnim format will reconstruct these frames using linear interpolation",
+        # As part of exporting to castle-anim-frames, we export each still
+        # frame to X3D. We iterate over all animation frames, from the start,
+        # exporting it and skipping this number of following frames.
+        # Smaller values mean less files (less disk usage, faster animation
+        # loading in game) but also worse quality (as castle-anim-frames loader in game
+        # only interpolates linearly between frames). Default is 4, which
+        # means every 5th frame is exported, which means 5 frames for each
+        # second (for default 25fps)
+        description="How many frames to skip between exported frames. The game using castle-anim-frames format will reconstruct these frames using linear interpolation",
             default=4, min=0, max=50)
 
     # properies passed through to the X3D exporter,
@@ -132,16 +133,16 @@ class ExportKAnim(bpy.types.Operator):
 
     path_mode = path_reference_mode
 
-    def output_frame(self, context, kanim_file, frame, frame_start):
+    def output_frame(self, context, output_file, frame, frame_start):
         """Output a given frame to a single X3D file, and add <frame...> line to
-        kanim file.
+        castle-anim-frames file.
 
         Arguments:
-        kanim_file    -- the handle to write xxx.kanim file,
+        output_file   -- the handle to write xxx.castle-anim-frames file,
                          to add <frame...> line.
         frame         -- current frame number.
         frame_start   -- the start frame number, used to shift frame times
-                         such that KAnim animation starts from time = 0.0.
+                         such that castle-anim-frames animation starts from time = 0.0.
         """
 
         # calculate filenames stuff
@@ -149,8 +150,8 @@ class ExportKAnim(bpy.types.Operator):
         output_basename = os.path.splitext(output_basename)[0] \
             + "_" + ("%04d" % frame) + '.x3d.gz'
 
-        # write kanim line
-        kanim_file.write('  <frame url="%s" time="%f" />\n' %
+        # write castle-anim-frames line
+        output_file.write('  <frame url="%s" time="%f" />\n' %
           (output_basename, (frame-frame_start) / 25.0))
 
         # write X3D with animation frame
@@ -171,20 +172,20 @@ class ExportKAnim(bpy.types.Operator):
             path_mode           = self.path_mode)
 
     def execute(self, context):
-        kanim_file = open(self.filepath, 'w')
-        kanim_file.write('<?xml version="1.0"?>\n')
-        kanim_file.write('<animation>\n')
+        output_file = open(self.filepath, 'w')
+        output_file.write('<?xml version="1.0"?>\n')
+        output_file.write('<animation>\n')
 
         frame = context.scene.frame_start
         while frame < context.scene.frame_end:
-            self.output_frame(context, kanim_file, frame, context.scene.frame_start)
+            self.output_frame(context, output_file, frame, context.scene.frame_start)
             frame += 1 + self.frame_skip
         # the last frame should be always output, regardless if we would "hit"
         # it with given frame_skip.
-        self.output_frame(context, kanim_file, context.scene.frame_end, context.scene.frame_start)
+        self.output_frame(context, output_file, context.scene.frame_end, context.scene.frame_start)
 
-        kanim_file.write('</animation>\n')
-        kanim_file.close()
+        output_file.write('</animation>\n')
+        output_file.close()
 
         return {'FINISHED'}
 
@@ -198,23 +199,23 @@ class ExportKAnim(bpy.types.Operator):
             else:
                 blend_filepath = os.path.splitext(blend_filepath)[0]
 
-            self.filepath = blend_filepath + ".kanim"
+            self.filepath = blend_filepath + ".castle-anim-frames"
 
         context.window_manager.fileselect_add(self)
         return {'RUNNING_MODAL'}
 
 def menu_func(self, context):
     self.layout.operator_context = 'INVOKE_DEFAULT'
-    self.layout.operator(ExportKAnim.bl_idname, text=ExportKAnim.bl_label)
+    self.layout.operator(ExportCastleAnimFrames.bl_idname, text=ExportCastleAnimFrames.bl_label)
 
 def register():
-    bpy.utils.register_class(ExportKAnim)
+    bpy.utils.register_class(ExportCastleAnimFrames)
     bpy.types.INFO_MT_file_export.append(menu_func)
 
 def unregister():
-    bpy.utils.unregister_class(ExportKAnim)
+    bpy.utils.unregister_class(ExportCastleAnimFrames)
     bpy.types.INFO_MT_file_export.remove(menu_func)
 
 if __name__ == "__main__":
     register()
-    bpy.ops.export.kanim('INVOKE_DEFAULT')
+    bpy.ops.export.castle_anim_frames('INVOKE_DEFAULT')
